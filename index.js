2b40479d0b197ae7a18672a20996e69a94593b37
twistedx <smgowing@hotmail.com>
2019-03-24 15:47:16 -0700

    my first web server

---
+++ index.js
@@ -0,0 +1,84 @@
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+//   if (req.url === "/") {
+//     fs.readFile(
+//       path.join(__dirname, "public", "index.html"),
+//       (err, content) => {
+//         if (err) throw err;
+
+//         res.writeHead(200, { "Content-Type": "text/html" });
+//         res.end(content);
+//       }
+//     );
+//   }
+
+
+// if (req.url === "/about") {
+//     fs.readFile(
+//       path.join(__dirname, "public", "about.html"),
+//       (err, content) => {
+//         if (err) throw err;
+
+//         res.writeHead(200, { "Content-Type": "text/html" });
+//         res.end(content);
+//       }
+//     );
+//   }
+
+//Build filepath
+let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+
+    //Extension
+  // Extension of file
+  let extname = path.extname(filePath);
+
+  // Initial content type
+  let contentType = "text/html";
+
+  // Check ext and set content type
+  switch (extname) {
+    case ".js":
+      contentType = "text/javascript";
+      break;
+    case ".css":
+      contentType = "text/css";
+      break;
+    case ".json":
+      contentType = "application/json";
+      break;
+    case ".png":
+      contentType = "image/png";
+      break;
+    case ".jpg":
+      contentType = "image/jpg";
+      break;
+  }
+
+  // read file
+  fs.readFile(filePath, (err, content) => {
+    if (err){
+        if(err.code == 'ENOENT'){
+            //page not found
+            fs.readFile(path.join(__dirname, 'public', '404.html'),
+            (err, content) => {
+                res.writeHead(200, { "Content-Type": "text/html" });
+                res.end(content, 'utf8');
+            });
+        } else {
+            //some server error
+            res.writeHead(500);
+            res.end(`Server Error: ${err.code}`);
+        }
+    } else {
+        //success
+        res.writeHead(200, { "Content-Type": contentType });
+        res.end(content, 'utf8');
+    }
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
